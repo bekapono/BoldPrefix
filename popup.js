de03a31662de6751f4ea366792eb2cb45344ffcf
Braulio Viveros <trotopype.dev@gmail.com>
2025-05-18 19:18:21 -0700

    added a second button; broken script

--- popup.js
+++ popup.js
@@ -11,3 +11,17 @@ document.getElementById('makeBold').addEventListener('click', async () => {
     console.error('Error executing script:', error);
   }
 });
+
+document.getElementById('revertBold').addEventListener('click', async () => {
+  try {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    if (chrome.scripting && chrome.scripting.executeScript) {
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['anticontent.js'],
+      });
+    }
+  } catch (error) {
+    console.error('Error executing script:', error);
+  }
+});
