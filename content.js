3ef1858f8a502afc7eab61b15ebe4c82f01807d1
bekapono <bsiliezar2@gmail.com>
2025-05-19 16:59:40 -0700

    feat: updated 'walk' function to prevent empty TEXT_NODE(s) and console logs for debugging

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+// Future Enhancements: if a word only consists of two letters bold whole word.
 function makeFrontHalfBold(text) {
   return text.replace(/\b\w+\b/g, (word) => {
     const half = Math.ceil(word.length / 2);
@@ -5,20 +6,28 @@ function makeFrontHalfBold(text) {
   });
 }
 
+// This functions goes through
 function walk(node) {
-  if (node.nodeType === 3) {
+  // Checks if node type is TEXT_NODE, and the text is empty
+  if (node.nodeType === 3 && node.nodeValue.trim().length !== 0) {
+    console.log('Mutating: ', node)  // remove when program is public
+
     const wrapper = document.createElement('span');
     wrapper.innerHTML = makeFrontHalfBold(node.nodeValue);
+    // .innerHTML flattens the div
+    console.log('Post-Mutation: ', wrapper.innerHTML) // remove when program is public
+
+    // .replaceChild removes the previous text with the mutated text.
     node.parentNode.replaceChild(wrapper, node);
-  } else if (
-    node.nodeType === 1 &&
-    node.nodeName !== 'SCRIPT' &&
-    node.nodeName !== 'STYLE'
-  ) {
+  }
+  else if (node.nodeType === 1 && node.nodeName !== 'SCRIPT' && node.nodeName !== 'STYLE') {
+
     for (let i = 0; i < node.childNodes.length; i++) {
+      //console.log("None TEXT_NODE", node, node.nodeValue) // remove when program is public
       walk(node.childNodes[i]);
     }
   }
 }
 
+console.log("BoldPrefix mutation start.\n")
 walk(document.body);
